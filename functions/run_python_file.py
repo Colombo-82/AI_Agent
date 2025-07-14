3f2e86f7853fc20b8495a8008703691281a74e5d
Colombo-82 <carlos.colombo@outlook.com>
2025-07-13 21:33:20 -0600

    Run Python

---
+++ functions/run_python_file.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_file.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(target_file):
+        return f'Error: File "{file_path}" not found.'
+    if not target_file.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    try:
+        result = subprocess.run(['python', target_file], capture_output=True, timeout=30, text=True, cwd=working_directory)
+        output = f"STDOUT:{result.stdout}\nSTDERR:{result.stderr}"
+        if result.returncode != 0:
+            output += f"\nProcess exited with code {result.returncode}"
+        if not result.stdout.strip() and not result.stderr.strip():
+            output = "No output produced."
+        return output
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+    
--- tests.py
+++ tests.py
@@ -1,24 +1,10 @@
-from functions.write_file import write_file
-
+from functions.run_python_file import run_python_file
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print("Result for write_file('calculator', 'lorem.txt', 'wait, this isn't lorem ipsum')")
-    print(result)
-    print("")
-
-    print("----------------------------------------------------------------------------------------")
-
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print("Result for write_file('calculator', 'pkg/morelorem.txt', 'lorem ipsum dolor sit amet')")
-    print(result)
-
-    print("----------------------------------------------------------------------------------------")
-
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print("Result for write_file('calculator', '/tmp/temp.txt', 'this should not be allowed')")
-    print(result)
-
+    print(run_python_file("calculator", "main.py"))
+    print(run_python_file("calculator", "tests.py"))
+    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file("calculator", "nonexistent.py"))
 
 if __name__ == "__main__":
     test()
